42e976965a47b25d9abf2e058e7cde236333025e
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2025-03-24 16:05:20 +0100

    fully functional

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,4 +1,5 @@
 // import postgres from 'postgres';
+// import { invoices } from '../lib/placeholder-data';
 
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -13,14 +14,44 @@
 // 	return data;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+
+
+
+// export async function GET() {
+//    return Response.json({
+//     message:
+//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   });
+//   try {
+//   	return Response.json(await listInvoices());
+//   } catch (error) {
+//   	return Response.json({ error }, { status: 500 });
+//   }
+// }
+
+
+import postgres from 'postgres';
+import { invoices } from '../lib/placeholder-data';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function listInvoices(
+) {const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+    return data;
+}
+
+export async function GET(
+) {
+  try {
+    const invoiceData = await listInvoices();
+    return Response.json(invoiceData);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
